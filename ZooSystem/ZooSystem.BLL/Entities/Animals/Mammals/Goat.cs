770e22834de7806d89bfdda57df0d6331e848237
Kristiyan Bashev <kristiyan.b@sbtech.com>
2017-02-17 17:03:32 +0200

    Add animal species and keeper improvements

---
+++ ZooSystem/ZooSystem.BLL/Entities/Animals/Mammals/Goat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooSystem.BLL.Entities.Animals.Mammals
+{
+    class Goat
+    {
+    }
+}
--- ZooSystem/ZooSystem.BLL/Entities/Keepers/Keeper.cs
+++ ZooSystem/ZooSystem.BLL/Entities/Keepers/Keeper.cs
@@ -1,10 +1,12 @@
 ﻿namespace ZooSystem.BLL.Entities.Keepers
 {
+    using Animals;
     using System;
     using System.Collections.Generic;
 
     public class Keeper
     {
-
+        private string Name;
+        private ICollection<Animal> animalsToGuard;
     }
 }
